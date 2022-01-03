0c81ba019d2d1c674342d4d405701711324bf765
Abdo2711-coder <abdelrahmandiv@gmail.com>
2022-01-03 03:22:22 +0200

    updates

--- src/pages/Collocations.js
+++ src/pages/Collocations.js
@@ -1,31 +1,116 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-// Translation
+import React, { useMemo, useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import Loading from "../components/Table/Loading"
+import { Card, Col, Form, Row } from "react-bootstrap";
+import DataTable, { createTheme } from "react-data-table-component";
+import { useSelector } from "react-redux";
+import { itemSlideUp } from "../helpers/Animation";
 
-
-// import {
-//   slideUp,
-//   animateList,
-// } from '../helpers/Animation'
-// import { motion } from 'framer-motion'
-// import ScrollReveal from '../components/ScrollReveal'
-
-
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 const Home = () => {
-	// const { t } = useTranslation();
+	const { t } = useTranslation();
+	const [filter, setfilter] = useState('');
+	const [Data, setData] = useState([]);
+	const [AllData, setAllData] = useState([]);
+	const [loading, setloading] = useState(true);
+	const { config } = useSelector((state) => state);
+	createTheme(
+		"solarized",
+		{
+			text: {
+				primary: "#268bd2",
+				secondary: "#268bd2",
+			},
+			background: {
+				default: "#222738",
+			},
+		},
+		"dark"
+	);
+	const columns = useMemo(
+		() => [
+			{
+				name: 'id',
+				selector: (row) => row.ID,
+				sortable: true,
+			},
+			{
+				name: `${t("Name")}`,
+				selector: (row) => row.AbnoSens || "text",
+				sortable: true,
+			},
+			{
+				name: 'name by arapy',
+				selector: (row) => "name by araby",
+				sortable: true,
+			},
+		],
+		[t],
+	);
+	// handle filter
+	useEffect(() => {
+		const newData = AllData?.filter((item) => {
+			return item?.ID.toString().includes(filter?.toLowerCase()) ||
+				false;
+		});
+		setData(newData);
+	}, [AllData, filter]);
 
+	useEffect(() => {
+		(async () => {
+			try {
+				await fetch("https://coldchain-api.herokuapp.com/api/warehouse/senswind")
+					.then(e => e.json())
+					.then(res => {
+						console.log(res)
+						setAllData([...res])
+						setloading(false)
+					})
+			} catch (error) {
+				console.log(error)
+			}
+		})();
+	}, []);
 	return (
-		<div>
-			{/* Boxes */}
-			<Row>
-				<Col lg="12">
-					<Row>
-						collocations
-					</Row>
-				</Col>
-			</Row>
-		</div>
+		<Card>
+			<Card.Body>
+				<Row>
+					<Col lg="12">
+						<div className="d-flex justify-content-between mt-3">
+							<Form.Group className="form-group col-6 col-lg-4">
+								<Form.Control
+									value={filter}
+									type="search"
+									id="search"
+									name="search"
+									onChange={(e) => setfilter(e.target.value)}
+									placeholder={`${t("Search")}...`}
+								/>
+							</Form.Group>
+						</div>
+					</Col>
+					<motion.div
+						variants={itemSlideUp}
+						initial="hidden"
+						animate="visible"
+						className="col"
+					>
+						{!loading ? (
+							<DataTable
+								title="Collocations"
+								columns={columns}
+								data={Data}
+								highlightOnHover
+								theme={`${config.darkMode && "solarized"}`}
+								pagination
+							/>
+						) :
+							<Loading />
+						}
+					</motion.div>
+				</Row>
+			</Card.Body>
+		</Card>
 	);
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 // Translation
 
 
@@ -16,13 +17,37 @@ const Home = () => {
   // const { t } = useTranslation();
 
   return (
-    <div>
-      {/* Boxes */}
-      <Row>
-        <Col lg="12">
-          <Row>
-            titles
-          </Row>
+    <div className="bd-example">
+      <Row className="row-cols-1 row-cols-md-2 g-4">
+        <Col sm={12} md={6} lg={4}>
+          <Card className="mb-0">
+            <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Past Tense" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6c757d"></rect><text x="30%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+            <Card.Body>
+              <h5 className="card-title">Past Tense</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <Link to="/" className="btn btn-primary">Details</Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <Card className="mb-0">
+            <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Present Tense" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#8a92a6"></rect><text x="30%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+            <Card.Body>
+              <h5 className="card-title">Present Tense</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <Link to="/" className="btn btn-primary">Details</Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <Card className="mb-0">
+            <svg className="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Future tense" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#222750"></rect><text x="30%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+            <Card.Body>
+              <h5 className="card-title">Future tense</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <Link to="/" className="btn btn-primary">Details</Link>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </div>
